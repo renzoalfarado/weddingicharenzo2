00589d7822b2f30d1797bf7cc393e9526b966a06
renzoalfarado <yakaligakuy97@gmail.com>
2021-09-20 13:13:34 +0700

    commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wedding-invitation
+Website Undangan Pernikahan
---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('#banner').backstretch([
+      "https://i.gyazo.com/6ad56047da856ab9d3998d0bd322ba09.jpg"
+    , "https://i.gyazo.com/e70f8f33eddbd721bf749f6b80adf28d.jpg"
+    , "https://i.gyazo.com/a5c7cd6a4a7d9a6a60783c4e1d478461.jpg"
+    , "https://i.gyazo.com/48a4651a3259530246bbe7ed3b08c17e.jpg"
+	, "https://i.gyazo.com/cc7df4558e9ef5322f807b75dbe29573.jpg"
+	, "https://i.gyazo.com/bf79b7ddd80a1daa5421a387b52b8638.jpg"
+	
+    ], {duration: 3000, fade: 750});
+});
